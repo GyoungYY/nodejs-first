841ce02165449adb849bd27e9ec063ef26f10f7d
yangguang <yangguang@bilibili.com>
2018-04-03 12:10:11 +0800

    nodejs回调函数

---
+++ main.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+
+//阻塞代码
+var data = fs.readFileSync('input.txt');
+
+console.log(data.toString());
+console.log('程序执行结束');
+
+console.log('---------');
+
+//非阻塞代码
+fs.readFile('input.txt',function (err,data) {
+    if(err){
+        return console.error(err);
+    }
+    console.log(data.toString());
+});
+console.log('程序执行结束');
